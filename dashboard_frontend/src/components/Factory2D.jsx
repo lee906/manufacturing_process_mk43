81375641bfe38293dd753067c4db6dfff028a21b
Blooming2081 <140838667+Blooming2081@users.noreply.github.com>
2025-06-19 14:56:10 +0900

    feat: 2D twin 수정 (#7)
    * feat: 테스트용 2d공장
    
    * Merge remote-tracking branch 'upstream/HEAD'
    
    * Merge remote-tracking branch 'upstream/main'
    
    * Merge remote-tracking branch 'upstream/main'
    
    * bugfix: java 경로 지정, 안 올라가게 ignore 수정
    
    * Merge remote-tracking branch 'upstream/main'
    
    * feat: 2D twin 수정

--- dashboard_frontend/src/components/Factory2D.jsx
+++ dashboard_frontend/src/components/Factory2D.jsx
@@ -1,34 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import Factory2DTwin from "./Factory2DTwin";
 
 const Factory2D = () => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-
-    const ctx = canvas.getContext('2d');
-
-    // 수직 컨베이어 벨트
-    for (let x = 130; x <= 1330; x += 400) {
-      ctx.fillStyle = 'gray';
-      ctx.fillRect(x, 100, 40, 700);
-    }
-
-    // 수평 컨베이어 벨트
-    for (let i = 0; i < 3; i++) {
-      ctx.fillStyle = 'gray';
-      ctx.fillRect(200 + (i * 400), i % 2 === 0 ? 30 : 830, 300, 40);
-    }
-
-  // 상단과 하단의 녹색 사각형들
-    for (let x = 100; x <= 1300; x += 400) {
-      ctx.fillStyle = 'green';
-      ctx.fillRect(x, 0, 100, 100);    // 상단 사각형    
-      ctx.fillRect(x, 800, 100, 100);  // 하단 사각형
-    }
-  }, []);
-
+ 
   return (
     <div className="row g-3">
       <div className="col-sm-6 col-lg-4">
@@ -101,16 +74,7 @@ const Factory2D = () => {
       <div className="col-sm-6 col-lg-8">
         <div className="card">
           <div className="card-body" style={{ height: "50rem" }}>
-            <canvas
-              ref={canvasRef}
-              width="1500"
-              height="900"
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'contain'
-              }}
-            />
+            <Factory2DTwin />
           </div>
         </div>
       </div>
