57a89fb6090d6079065a6ea1d8b6e7a703d8adb7
chmise <114454630+chmise@users.noreply.github.com>
2025-06-27 12:49:37 +0900

    Chore/misc updates (#14)
    * chore: 파일명 수정
    
    * refactor: 공정 구조 개편
    
    * refactor: lombok 코드로 변경
    
    * refactor: 랜덤 -> 시계열 데이터

--- dashboard_backend/src/main/java/com/u1mobis/dashboard_backend/entity/User.java
+++ dashboard_backend/src/main/java/com/u1mobis/dashboard_backend/entity/User.java
@@ -6,50 +6,41 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 요구사항
+@ToString(exclude = {"password"}) // 보안상 password 제외
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // ID만으로 비교
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    @EqualsAndHashCode.Include // equals/hashCode에 ID만 포함
+    private Long userId;
 
-    @Column(unique = true)
-    private String username;
+    @Column(name = "company_id", nullable = false)
+    private Long companyId;
 
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
     private String password;
 
-    // 기본 생성자
-    public User() {}
-
-    // 모든 필드를 받는 생성자
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    // id
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    // username
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    // password
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
+    // 비즈니스 로직에 필요한 생성자만 명시적으로 생성
+    public User(Long companyId, String userName, String password) {
+        this.companyId = companyId;
+        this.userName = userName;
         this.password = password;
     }
 }
